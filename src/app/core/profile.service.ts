3b0499e8b05691b38fb6fcd5ac9ad3d4261f4995
unforbidable <unforbidable@gmail.com>
2022-05-14 17:52:41 +0200

    Angular 13

--- src/app/core/profile.service.ts
+++ src/app/core/profile.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Profile } from '../models/profile.model';
-import { stringify } from 'querystring';
 import { Alloy } from '../models/alloy.model';
 
 @Injectable({
